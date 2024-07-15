b3e5b5bfc9fade4efe6d7e179d61e09c3d409a52
Rahil Tariq <163430192+rahiltariq07@users.noreply.github.com>
2024-07-15 12:04:46 +0530

    Add files via upload

---
+++ WebApplication2/WebApplication2/User.cs
@@ -0,0 +1,10 @@
+﻿namespace WebApplication2
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Job { get; set; }
+    }
+}
